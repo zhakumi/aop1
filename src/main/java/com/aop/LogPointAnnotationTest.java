5f8b2d9b8d3e58e47544c00940521616f7d7ad75
can.wang@greatonce.com <wangcan1230>
2018-05-05 16:52:14 +0800

    优化

--- src/main/java/com/aop/LogPointAnnotationTest.java
+++ src/main/java/com/aop/LogPointAnnotationTest.java
@@ -11,21 +11,22 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 public class LogPointAnnotationTest {
     @Autowired
     private LogPoint feature3;
+    @Autowired
+    private LogPoint feature1;
 
     @Test
     public void log(){
-        feature3.log();
+        feature1.log();
     }
 
     @Test
     public void throwLog() throws Exception{
-        feature3.throwLog();
+        feature1.throwLog();
     }
     @Test
     public void around(){
         feature3.around();
     }
-
 
 }
 
